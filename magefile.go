8147b010684df362f33d5c4903dad913917f62b5
Carolyn Van Slyck <me@carolynvanslyck.com>
2022-03-29 13:02:51 -0500

    Run tests on pull requests and merges
    This adds a github workflow that tests the code when a pull request
    is submitted or merged.
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- magefile.go
+++ magefile.go
@@ -4,10 +4,16 @@
 package main
 
 import (
+	"get.porter.sh/magefiles/ci"
+	"github.com/carolynvs/magex/mgx"
 	"github.com/carolynvs/magex/shx"
 )
 
 var must = shx.CommandBuilder{StopOnError: true}
+
+func ConfigureAgent() {
+	mgx.Must(ci.ConfigureAgent())
+}
 
 func Build() {
 	must.RunV("go", "build", "./...")
