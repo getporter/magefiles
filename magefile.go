4a449a7edc381a3c5d3df13a45c3e1783c12293f
Kim Christensen <kmch@simcorp.com>
2024-08-30 22:44:17 +0200

    chore: Add SetupDCO target
    Signed-off-by: Kim Christensen <kmch@simcorp.com>

--- magefile.go
+++ magefile.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"get.porter.sh/magefiles/ci"
+	"get.porter.sh/magefiles/git"
 	"get.porter.sh/magefiles/porter"
 	"get.porter.sh/magefiles/tools"
 	"github.com/carolynvs/magex/mgx"
@@ -37,4 +38,10 @@ func Test() {
 	mg.SerialDeps(porter.UseBinForPorterHome, porter.EnsurePorter)
 
 	must.RunV("go", "test", "./...")
+}
+
+// SetupDCO configures your git repository to automatically sign your commits
+// to comply with our DCO
+func SetupDCO() error {
+	return git.SetupDCO()
 }
